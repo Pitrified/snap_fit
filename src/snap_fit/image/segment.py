0999579f9b294074d4d49d4c61df5cd9cd9a9c2e
Pitrified <nobilipietro@gmail.com>
2025-12-13 22:03:48 +0100

    Move shape computation in Segment init

--- src/snap_fit/image/segment.py
+++ src/snap_fit/image/segment.py
@@ -6,6 +6,10 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 
+from snap_fit.config.types import SegmentShape
+from snap_fit.image.process import estimate_affine_transform
+from snap_fit.image.process import transform_contour
+
 if TYPE_CHECKING:
     from snap_fit.image.contour import Contour
 
@@ -39,6 +43,8 @@ class Segment:
         self.coords = np.vstack((self.start_coord, self.end_coord))
         self.swap_coords = np.flip(self.coords, axis=0)
 
+        self._compute_shape()
+
     def get_points(self) -> None:
         """Get the points of the segment.
 
@@ -60,6 +66,41 @@ class Segment:
             # if the segment does not wrap around the contour,
             # get the points from the start to the end
             self.points = self.contour.cv_contour[self.start_idx : self.end_idx + 1]
+
+    def _compute_shape(self) -> None:
+        """Compute the shape of this segment."""
+        # transform the contour on x axis
+        source = self.coords
+        target = np.array([[0, 0], [0, 500]])
+        transform_matrix = estimate_affine_transform(source, target)
+        points_transformed = transform_contour(self.points, transform_matrix)
+
+        # get only x coords
+        s1_xs = points_transformed[:, 0, 0]
+
+        # count how many points are far from the center line
+        flat_th = 20
+        out_count = (s1_xs < -flat_th).sum()
+        in_count = (s1_xs > flat_th).sum()
+
+        # check if more than a certain number of points are far from the line
+        count_th = 5
+        is_out = bool(out_count > count_th)
+        is_in = bool(in_count > count_th)
+
+        # decide the shape
+        match (is_out, is_in):
+            case True, False:
+                self.shape = SegmentShape.OUT
+            case False, True:
+                self.shape = SegmentShape.IN
+            case False, False:
+                self.shape = SegmentShape.EDGE
+            case True, True:
+                self.shape = SegmentShape.WEIRD
+            case _:
+                msg = "how did you manage this?"
+                raise ValueError(msg)
 
     def __len__(self) -> int:
         """Return the number of points in the segment."""
